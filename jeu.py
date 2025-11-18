b85bc5951e0b3cf30d86976dadd9b3b3b31d2177
Gael Pupin <gael@pupin.fr>
2025-11-18 13:14:08 +0100

    Refactor sql_conn.py to implement database connection and query functions; remove unused bonjour function and clean up jeu.py.

--- jeu.py
+++ jeu.py
@@ -1,4 +1,2 @@
 import sql_conn
 
-
-print(sql_conn.bonjour())
--- sql_conn.py
+++ sql_conn.py
@@ -1,15 +1,24 @@
 import sqlite3
 
-# load database
-db = sqlite3.connect('sport_manager.db')
-cursor = db.cursor()
+def init_connection():
+    conn = sqlite3.connect("sport_manager.db")
+    return conn
 
+def get_all_manageurs(conn):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM manageur")
+    return cursor.fetchall()
 
-def bonjour():
-    
+def get_equipes_by_manageur(conn, manageur_id):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM equipes WHERE id_manageur = ?", (manageur_id,))
+    return cursor.fetchall()
 
+def get_all_joueurs_by_equipe(conn, equipe_id):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM joueurs WHERE id_equipe = ?", (equipe_id,))
+    return cursor.fetchall()
 
-
-
-
-    return "Hello, World!"
+conn = init_connection()
+for i in get_all_joueurs_by_equipe(conn, get_equipes_by_manageur(conn, 1)[0][0]):
+    print(i)
