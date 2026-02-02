f3f569da359d56c381016e42510ac3692101eac4
Giry Eliott <eliott.giry@viacesi.fr>
2026-02-02 11:03:50 +0100

    Initialize localStorage with default player and team data

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const monStockage = localStorage;
 
+monStockage.setItem(
+  "Joueur",
+  JSON.stringify([{ nom: "Nom", prenom: "Prénom", stats: {} }]),
+);
+monStockage.setItem(
+  "Équipes",
+  JSON.stringify([{ nom: "Nom", manager: "Manager", stats: {} }]),
+);
+monStockage.setItem("Joueurs", JSON.stringify([{ nombre: "Nombre" }]));
+
+const joueur = JSON.parse(monStockage.getItem("Joueur"));
+const equipes = JSON.parse(monStockage.getItem("Équipes"));
+const joueurs = JSON.parse(monStockage.getItem("Joueurs"));
